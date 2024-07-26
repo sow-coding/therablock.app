5a4599bcc15de06273db1c6777c7e54b254d5d2b
Saikou Sow <sow.coding@gmail.com>
2024-07-26 17:10:04 +0200

    almost very lean but sudo-prompt error

--- main.js
+++ main.js
@@ -1,15 +1,10 @@
 const { app, BrowserWindow, ipcMain, dialog, Menu } = require('electron');
 const path = require('path');
 const sudo = require('sudo-prompt');
-const log = require('electron-log');
 
 const options = {
   name: 'Therablock',
 };
-
-log.transports.file.resolvePath = () => path.join(app.getPath('userData'), 'logs', 'main.log');
-
-log.info('App starting...');
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -29,19 +24,16 @@ function createWindow() {
     win.loadURL("http://localhost:3000/home");
   }
 
-  log.info('Window created and loaded');
 }
 
 app.on('window-all-closed', () => {
   log.info('All windows closed');
   if (process.platform !== 'darwin') {
-    log.info('Quitting app as all windows are closed');
     app.quit();
   }
 });
 
 app.on('activate', () => {
-  log.info('App activated');
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
@@ -79,16 +71,7 @@ ipcMain.on('schedule-block-app', (event, { apps, start, end, daysOfWeek }) => {
   apps.forEach(app => {
     const setupCommand = `powershell -File "${path.join(scriptDirectory, 'setupAppTasks.ps1')}" -appName "${app.trim()}" -startHour ${start.hour} -startMinute ${start.minute} -endHour ${end.hour} -endMinute ${end.minute} -daysOfWeek "${daysOfWeek}"`;
 
-    log.info(`Setting up schedule block for ${app} with command: ${setupCommand}`);
-
-    sudo.exec(setupCommand, options, (error, stdout, stderr) => {
-      if (error) {
-        log.error(`Error setting up scheduled tasks for ${app}: ${error.message}`);
-        return;
-      }
-      log.info(`Scheduled tasks set up for app: ${app}`);
-      log.info(stdout);
-    });
+    sudo.exec(setupCommand, options);
   });
 });
 
@@ -97,50 +80,40 @@ ipcMain.on('get-scheduled-tasks', (event) => {
     ? path.join(process.resourcesPath, 'scripts', 'getScheduledTasks.ps1')
     : path.join(__dirname, 'scripts', 'getScheduledTasks.ps1');
   
-  log.info(`Getting scheduled tasks with script: ${scriptPath}`);
   
   sudo.exec(`powershell -File "${scriptPath}"`, (error, stdout, stderr) => {
     if (error) {
-      log.error(`Error retrieving scheduled tasks: ${error.message}`);
       event.reply('scheduled-tasks-response', { success: false, error: error.message });
       return;
     }
     if (stderr) {
-      log.error(`Error retrieving scheduled tasks: ${stderr}`);
       event.reply('scheduled-tasks-response', { success: false, error: stderr });
       return;
     }
-    log.info('Scheduled tasks retrieved successfully');
     event.reply('scheduled-tasks-response', { success: true, data: JSON.parse(stdout) });
   });
 });
 
 ipcMain.on('update-task', (event, { oldTask, newTask }) => {
   const { blockTaskName, unblockTaskName } = oldTask;
-  log.info(`Attempting to delete old tasks: ${blockTaskName} and ${unblockTaskName}`);
 
   const deleteBlockCommand = `powershell -Command "Unregister-ScheduledTask -TaskName '${blockTaskName}' -Confirm:\$false"`;
   const deleteUnblockCommand = `powershell -Command "Unregister-ScheduledTask -TaskName '${unblockTaskName}' -Confirm:\$false"`;
 
   sudo.exec(deleteBlockCommand, options, (error, stdout, stderr) => {
     if (error) {
-      log.error(`Error deleting old block task: ${error.message}`);
       event.reply('update-task-response', { success: false, error: error.message });
       return;
     }
-    log.info(`Old block task deleted: ${blockTaskName}`);
 
     sudo.exec(deleteUnblockCommand, options, (error, stdout, stderr) => {
       if (error) {
-        log.error(`Error deleting old unblock task: ${error.message}`);
         event.reply('update-task-response', { success: false, error: error.message });
         return;
       }
-      log.info(`Old unblock task deleted: ${unblockTaskName}`);
 
       // Ensure daysOfWeek is not empty
       if (!newTask.daysOfWeek || newTask.daysOfWeek.length === 0) {
-        log.error('Days of week are required to set up a new task.');
         event.reply('update-task-response', { success: false, error: 'Days of week are required to set up a new task.' });
         return;
       }
@@ -151,16 +124,11 @@ ipcMain.on('update-task', (event, { oldTask, newTask }) => {
 
       const setupCommand = `powershell -File "${path.join(scriptDirectory, 'setupTasks.ps1')}" -site ${newTask.site} -startHour ${newTask.start.hour} -startMinute ${newTask.start.minute} -endHour ${newTask.end.hour} -endMinute ${newTask.end.minute} -daysOfWeek "${newTask.daysOfWeek}"`;
 
-      log.info(`Setting up new task with command: ${setupCommand}`);
-
       sudo.exec(setupCommand, options, (error, stdout, stderr) => {
         if (error) {
-          log.error(`Error setting up new task: ${error.message}`);
           event.reply('update-task-response', { success: false, error: error.message });
           return;
         }
-        log.info(`Scheduled tasks updated for sites: ${newTask.sites}`);
-        log.info(stdout);
         event.reply('update-task-response', { success: true });
       });
     });
@@ -171,24 +139,11 @@ ipcMain.on('delete-task', (event, taskName) => {
   const blockTaskName = `BlockSite_${taskName}`;
   const unblockTaskName = `UnblockSite_${taskName}`;
 
-  log.info(`Deleting tasks: ${blockTaskName} and ${unblockTaskName}`);
-
   const deleteBlockCommand = `powershell -Command "Unregister-ScheduledTask -TaskName '${blockTaskName}' -Confirm:\$false"`;
   const deleteUnblockCommand = `powershell -Command "Unregister-ScheduledTask -TaskName '${unblockTaskName}' -Confirm:\$false"`;
 
   sudo.exec(deleteBlockCommand, options, (error, stdout, stderr) => {
-    if (error) {
-      log.error(`Error deleting block task: ${error.message}`);
-      return;
-    }
-    log.info(`Block task deleted: ${blockTaskName}`);
 
-    sudo.exec(deleteUnblockCommand, options, (error, stdout, stderr) => {
-      if (error) {
-        log.error(`Error deleting unblock task: ${error.message}`);
-        return;
-      }
-      log.info(`Unblock task deleted: ${unblockTaskName}`);
-    });
+    sudo.exec(deleteUnblockCommand, options);
   });
 });
